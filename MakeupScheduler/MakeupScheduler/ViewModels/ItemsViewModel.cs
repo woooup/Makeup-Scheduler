5d0d5dd4b6f49aa87c0171c58a8567c9cd2727ca
stefan.vasic <vasic2710@gmail.com>
2019-01-29 00:39:10 +0100

    Simple report added..

--- MakeupScheduler/MakeupScheduler/ViewModels/ItemsViewModel.cs
+++ MakeupScheduler/MakeupScheduler/ViewModels/ItemsViewModel.cs
@@ -13,37 +13,26 @@ namespace MakeupScheduler.ViewModels
     public class ItemsViewModel : BaseViewModel
     {
         private DateTime selectedFilter = DateTime.MinValue;
-
         public ObservableRangeCollection<Item> Items { get; set; }
         public ObservableRangeCollection<Item> AllItems { get; set; }
         public ObservableRangeCollection<DateTime> FilterOptions { get; }
-        public DateTime SelectedFilter
-        {
-            get => selectedFilter;
-            set
-            {
-                if (SetProperty(ref selectedFilter, value))
-                    FilterItems();
-            }
-        }
-
-        public bool IsEnabled { get; set; }
+        public string report;
 
         public ItemsViewModel()
         {
             Items = new ObservableRangeCollection<Item>();
             AllItems = new ObservableRangeCollection<Item>();
-
             FilterOptions = new ObservableRangeCollection<DateTime>
             {
                 DateTime.MinValue
             };
             LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
-
             MessagingCenter.Subscribe<NewItemPage, Item>(this, "AddItem", async (obj, item) =>
             {
                 var newItem = item as Item;
+
                 AllItems.Add(newItem);
+
                 AddFilterOptions(newItem);
                 FilterItems();
 
@@ -60,20 +49,48 @@ namespace MakeupScheduler.ViewModels
                 await DataStore.UpdateItemAsync(newItem);
 
                 FilterItems();
+
             });
             MessagingCenter.Subscribe<NewItemPage, Item>(this, "DeleteItem", async (obj, item) =>
             {
                 var newItem = item as Item;
                 AllItems.Remove(newItem);
-                //AddFilterOptions(newItem);
-               
-
                 await DataStore.DeleteItemAsync(item.Id);
                 FilterItems();
             });
 
         }
 
+        public string Report
+        {
+            get => report;
+            set
+            {
+                if (SetProperty(ref report, value))
+                        OnPropertyChanged("Report");
+            }
+            
+        }
+
+        private void CalculateReport()
+        {
+            double price = 0;
+            if(Items.Count>0)
+                price = Items.Sum(x => x.Price);
+
+            Report = String.Format("Termini:{0}, Ukupno: {1} din.", Items.Count, price);
+        }
+
+        public DateTime SelectedFilter
+        {
+            get => selectedFilter;
+            set
+            {
+                if (SetProperty(ref selectedFilter, value))
+                    FilterItems();
+            }
+        }
+    
         private ObservableRangeCollection<Item> UpdateAllItems(ObservableRangeCollection<Item> allItems,Item item)
         {
             
@@ -84,8 +101,6 @@ namespace MakeupScheduler.ViewModels
 
         }
 
-        public Command LoadItemsCommand { get; set; }
-
         private void AddFilterOptions(Item newItem)
         {
             if (!FilterOptions.Contains(newItem.Date.Date))
@@ -95,7 +110,10 @@ namespace MakeupScheduler.ViewModels
         private void FilterItems()
         {
             Items.ReplaceRange(AllItems.OrderBy(y=>y.Date).ThenBy(x=>x.StartTime).Where(a => a.Date.Date == SelectedFilter.Date || SelectedFilter == DateTime.MinValue));
+            CalculateReport();
         }
+
+        public Command LoadItemsCommand { get; set; }
 
         private async Task ExecuteLoadItemsCommand()
         {
